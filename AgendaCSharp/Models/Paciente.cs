23145e6de0e01fe6f86efed8f359f91ddb9d455d
Lucas Chianca <lucaseduardochianca.dev@gmail.com>
2024-06-10 14:22:06 -0300

    Corrigido entidade 'paciente'

--- AgendaCSharp/Models/Paciente.cs
+++ AgendaCSharp/Models/Paciente.cs
@@ -2,4 +2,26 @@
 
 public class Paciente
 {
+    private string _cpf { get; }
+    private string _nome;
+    private DateTime _dataDeNascimento;
+
+    public Paciente(string cpf, string nome, DateTime dataDeNascimento)
+    {
+        _cpf = cpf;
+        _nome = nome;
+        _dataDeNascimento = dataDeNascimento;
+    }
+
+    public string Nome
+    {
+        get { return _nome; }
+        set { _nome = value; }
+    }
+
+    public DateTime DataDeNascimento
+    {
+        get { return _dataDeNascimento; }
+        set { _dataDeNascimento = value; }
+    }
 }

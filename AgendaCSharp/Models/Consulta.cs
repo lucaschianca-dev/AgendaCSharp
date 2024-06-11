e3bef6b0466289732998588cb8f514078c70a042
Lucas Chianca <lucaseduardochianca.dev@gmail.com>
2024-06-10 22:59:52 -0300

    Criado ConsultaRepository e adicionado metodos para listar e adicionar uma consulta

--- AgendaCSharp/Models/Consulta.cs
+++ AgendaCSharp/Models/Consulta.cs
@@ -2,4 +2,16 @@
 
 public class Consulta
 {
+    public string CpfPaciente { get; set; }
+    public DateTime Data { get; set; }
+    public TimeSpan HoraInicial { get; set; }
+    public TimeSpan HoraFinal { get; set; }
+
+    public Consulta(string cpfPaciente, DateTime data, TimeSpan horaInicial, TimeSpan horaFinal)
+    {
+        CpfPaciente = cpfPaciente;
+        Data = data;
+        HoraInicial = horaInicial;
+        HoraFinal = horaFinal;
+    }
 }
---
+++ AgendaCSharp/Repositories/ConsultaRepository.cs
@@ -0,0 +1,18 @@
+﻿using AgendaCSharp.Models;
+
+namespace AgendaCSharp.Repositories;
+
+public class ConsultaRepository
+{
+    private List<Consulta> _consultas = new List<Consulta>();
+
+    public void AdicionarConsulta(Consulta consulta)
+    {
+        _consultas.Add(consulta);
+    }
+
+    public List<Consulta> BuscarConsultas(Consulta consulta)
+    {
+        return _consultas;
+    }
+}
